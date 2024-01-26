b8481c913f1d16cc68e961791e8ddd725039103f
Nayeem Abdullah <nayeemabdullah00@gmail.com>
2024-01-26 05:52:13 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,52 @@
-// create web server
+// Create web server and API endpoints for comments
+// Dependencies: express, body-parser, fs, path
 
-const http = require('http');
+// Import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+// Create router
+const router = express.Router();
+
+// Set up body parser
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+// Set up file paths
+const commentsPath = path.join(__dirname, '../data/comments.json');
+
+// Create GET request for comments
+router.get('/', (req, res) => {
+  fs.readFile(commentsPath, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+// Create POST request for comments
+router.post('/', (req, res) => {
+  let comments = [];
+  fs.readFile(commentsPath, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      comments = JSON.parse(data);
+      comments.push(req.body);
+      fs.writeFile(commentsPath, JSON.stringify(comments), (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(comments);
+        }
+      });
+    }
+  });
 });
+
+// Export router
+module.exports = router;
